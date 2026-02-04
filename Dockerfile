4f946ed05ded4105e84bfb423174da49801ca654
Cortesja <jaredallencortes@gmail.com>
2026-02-04 16:46:12 +0900

    added the databases wrapper and changed the names

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,6 @@ COPY plugin_requirements.txt /opt/netbox/plugin_requirements.txt
 RUN --mount=type=cache,target=/root/.cache/uv \
     /usr/local/bin/uv pip install -r /opt/netbox/plugin_requirements.txt
 
+COPY configuration/wrap_databases.py /etc/netbox/config/wrap_databases.py
+COPY configuration/branching.py /etc/netbox/config/branching.py
 COPY configuration/plugins.py /etc/netbox/config/plugins.py
--- configuration/plugins.py
+++ configuration/plugins.py
@@ -1,6 +1,3 @@
-import os
-from netbox_branching.utilities import DynamicSchemaDict
-
 PLUGINS = [
     'netbox_acls',
     'netbox_floorplan',
@@ -10,22 +7,4 @@ PLUGINS = [
     'netbox_topology_views',
     'slurpit_netbox',
     'netbox_branching',
-]
-
-DATABASE = None
-
-DATABASES = DynamicSchemaDict({
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':     os.environ["DB_NAME"],          # Database name
-        'USER':     os.environ["DB_USER"],          # PostgreSQL username
-        'PASSWORD': os.environ["DB_PASSWORD"],      # PostgreSQL password
-        'HOST':     os.environ["DB_HOST"],          # Database server
-        'PORT':     os.environ["DB_PORT"],          # Database port (leave blank for default)
-        'CONN_MAX_AGE': 300,                        # Max database connection age
-    }
-})
-
-DATABASE_ROUTERS = [
-    'netbox_branching.database.BranchAwareRouter',
 ]
